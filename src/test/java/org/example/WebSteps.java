b873a834384a8451576ad6888ab6599396f1a965
ValeryIvlev <117769945+ValeryIvlev@users.noreply.github.com>
2023-03-16 09:15:27 +0300

    Update WebSteps.java

--- src/test/java/org/example/WebSteps.java
+++ src/test/java/org/example/WebSteps.java
@@ -17,33 +17,26 @@ public class WebSteps {
 
     @Step("Открываем главную страницу")
     public void openMainPage() {
-
         open("https://github.com");
-
     }
+    
     @Step("Поиск {repo}")
     public void sreachForRepo(String repo) {
-
         $(".header-search-input").click();
         $(".header-search-input").sendKeys(repo);
         $(".header-search-input").submit();
         $(linkText(repo)).click();
-
     }
+    
     @Step("Поиск {issue}")
     public void sreachIssue(int issue) {
 
         $("#issues-tab").click();
         $(withText("#" + issue)).should(Condition.exist);
-
     }
 
     @Attachment(value = "Srcreenshot", type ="image/png", fileExtension = "png")
     public byte[] attachScreen() {
-
         return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
-
     }
-
 }
-
